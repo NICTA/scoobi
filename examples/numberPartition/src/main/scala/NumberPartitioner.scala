727b472a9c92bfb76002b5a49d7f268f6d506199
Eric Springer <eric.springer@nicta.com.au>
2011-10-28 11:21:33 +1100

    Put examples in their own package

--- examples/numberPartition/src/main/scala/NumberPartitioner.scala
+++ examples/numberPartition/src/main/scala/NumberPartitioner.scala
@@ -13,6 +13,8 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
+package com.nicta.scoobi.examples
+
 import com.nicta.scoobi._
 import com.nicta.scoobi.Scoobi._
 import com.nicta.scoobi.io.text._
