957ebe270c077092a9c380fb11bdcaa0b8a6a275
etorreborre <etorreborre@yahoo.com>
2012-03-29 15:17:57 +1100

    more comments on the use of applicatives for scalacheck

--- src/test/scala/com/nicta/scoobi/data/Data.scala
+++ src/test/scala/com/nicta/scoobi/data/Data.scala
@@ -13,6 +13,14 @@ trait Data {
    * allow the use of the applicative syntax to build generators:
    *
    * (gen1 |@| gen2 |@| gen3)(function(_,_,_))
+   *
+   * It's especially useful when building generators for case classes
+   *
+   * import scalaz.Scalaz._
+   * case class MyClass(a: A, b: B, c: C)
+   *
+   * val MyCaseClassGenerator: Gen[MyCaseClass] = (genA |@| genB |@| genC)(MyCaseClass)
+   *
    */
   implicit def genIsApply: Apply[Gen] = new Apply[Gen] {
     def ap[A, B](fa: => Gen[A])(f: => Gen[(A) => B]) = fa.map2(f)((v, function) => function(v))
