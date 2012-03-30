b15e6028905620de8901e320212a4c401511b469
matthew <matthew@foursquare.com>
2012-03-30 19:39:18 -0400

    Filesystem.get calls now compatible with S3

--- src/main/scala/com/nicta/scoobi/impl/exec/Executor.scala
+++ src/main/scala/com/nicta/scoobi/impl/exec/Executor.scala
@@ -54,7 +54,7 @@ object Executor {
 
     /* Check that all output dirs don't already exist. */
     def pathExists(p: Path) = {
-      val s = FileSystem.get(Scoobi.conf).globStatus(p)
+      val s = FileSystem.get(p.toUri, Scoobi.conf).globStatus(p)
       if (s == null)          false
       else if (s.length == 0) false
       else                    true
