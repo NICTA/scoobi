d9719960efbc3b3263a97852fa344166b87ad2d4
Ben Lever <ben.lever@nicta.com.au>
2012-05-28 16:05:31 +1000

    Remove DelayedInit from ScoobiApp.
    Modify ScoobiApp to not mixin DelayedInit. Instead, a client must
    implement the 'run()' method if it mixes-in ScoobiApp.
    
    Previously, when using ScoobiApp, if the function passed to a DList
    method closed over a value, the value will not be initialised at the
    time when the function is run. This is an issue with the use of
    DelayedInit and hence the reason for dropping it.

--- src/main/scala/com/nicta/scoobi/ScoobiApp.scala
+++ src/main/scala/com/nicta/scoobi/ScoobiApp.scala
@@ -3,22 +3,18 @@ package com.nicta.scoobi
 import com.nicta.scoobi.Scoobi._
 import org.apache.hadoop.conf.Configuration
 
-trait ScoobiApp extends DelayedInit {
-
-  private var initCode: () => Unit = _
+trait ScoobiApp {
 
   protected var args: Array[String] = _
 
   protected implicit val configuration: ScoobiConfiguration = ScoobiConfiguration()
 
-  override def delayedInit(body: => Unit) {
-     initCode = (() => body)
-  }
+  def run()
 
   def main(arguments: Array[String]) {
     configuration.withHadoopArgs(arguments) { remainingArgs =>
       args = remainingArgs
-      initCode()
+      run()
     }
   }
 }
