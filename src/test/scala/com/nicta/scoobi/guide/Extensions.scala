8378ac545bea307a64e7f0317b06cc05fbb53a1e
Eric springer <ericwspringer@gmail.com>
2012-06-03 22:02:59 +1000

    Stab at documenting some of the scoobi extensions

--- src/test/scala/com/nicta/scoobi/guide/Extensions.scala
+++ src/test/scala/com/nicta/scoobi/guide/Extensions.scala
@@ -2,9 +2,44 @@ package com.nicta.scoobi.guide
 
 class Extensions extends ScoobiPage { def is = "Extensions".title ^
 """
-Coming soon!
+Scoobi includes a growing set of reusable components, ready to use in your application. They are in scoobi.lib, take a look at the API documentation
+to get a real sense on how to use them. 
+  
+### Joins
+  
+  [Background Reading](http://en.wikipedia.org/wiki/Join_%28relational_algebra%29#Joins_and_join-like_operators)
 
-### Joins and Co-Groups
+  Often you two collections, and would like to collate them somehow. While you might expect the logic to be around a `DList[A]`
+  a `DList[B]` and predicate `(A, B) => Bool` to create `DList[(A, B)]` -- it would not be possible to do (remotely) efficiently
+  in this manner, and be rather cumbersome. So instead our join code is structured around having a `DList[(K, A)]` and a `DList[(K, B)]`
+  where the K has to have an `Ordering` available (a requirement for `.groupByKey`)
+  
+  From this, you generally end up with a `DList[(K, (A, B)]`.
+  
+  In addition to a normal (equi)join, often you want to do something if there's no corresponding values in the first (left) or second (right)
+  dlist. These are the basis of left, right, inner and outter joins. See: [wikipedia](http://en.wikipedia.org/wiki/Join_%28SQL%29) for an almost
+  tutorial like walkthrough of the different join types.  
+  
+### Cogroup
+  
+  Sometimes when dealing a `DList[(K, (A, B))` where there's multiple K's, its nicer to have it form `DList[(K, (Iterable[A], Iterable[B]))]` so that
+  each K is unique. If Cogroup is what you need, it's a lot more efficient to use it then a relational join, followed by a `.groupByKey`
+  
+### DMatrix
+  
+  `DMatrix[Elem, V]` provides you with a distributed matrix currently backed by a coordinate oriented DList ( `DList[((Elem, Elem), V)]` ) which makes it
+  ideal for storing huge sparse matrixes. When multiplying by vectors that can fit in memory, it often makes sense to convert it to a RowWise or ColWise matrix
+  and use it from there. There are implicit conversions if the conversion only needs to once
+  
+### DVector
+  
+  `DVector[Elem, V]` is in the same spirit of a DMatrix, backed by a coordinate form DList ( `DList[(Elem, V)]` ) ideal for huge and spare vectors.
+
+### In Memory Vector
+  `InMemVector[Elem, V]` and `InMemDenseVector[V]` are for vectors that can fit into memory. They are based around a `DObject` which means a copy
+  gets efficiently sent to each mapper. For things like Matrix by Vector operations, this can result in a huge speedup. The obvious catch is, they
+  need to fit into memory.
+
 
 """
 
--- src/test/scala/com/nicta/scoobi/guide/QuickStart.scala
+++ src/test/scala/com/nicta/scoobi/guide/QuickStart.scala
@@ -6,23 +6,19 @@ class QuickStart extends ScoobiPage { def is = "Quick Start".title^
 
 Scoobi has the following requirements:
 
-* [Cloudera's Hadoop 0.20.2](http://www.cloudera.com/hadoop/)
-* [Scala 2.9.2](http://www.scala-lang.org/downloads): Note that this is typically set in build.sbt
+* [Cloudera's Hadoop 0.20.2 (CDH3)](http://www.cloudera.com/hadoop/)
 * [Sbt 0.11.3](https://github.com/harrah/xsbt/wiki)
+* [Scala 2.9.2](http://www.scala-lang.org/downloads): Note that this is typically set, and fetched with sbt
 
 Scala and Hadoop are obvious prerequisites. In addition, the Scoobi library and Scoobi applications use [sbt](https://github.com/harrah/xsbt/wiki) (version 0.11.3) for dependency management and building.
 
 **NOTE**: You will probably have to edit the `sbt` launcher script (located in `~/bin/sbt` or wherever `sbt` has been installed) to increase the maximum heap size, or you will get out-of-memory errors.  Try changing the existing `-Xmx` option to `-Xmx2048M` (or adding this option if it's not already present).  If this still leads to errors, `-Xmx4096M` should be enough.
 
+End users typically should avoid manually building scoobi, and use the prebuilt jars on sonatype.
 
 ### Building Word Count
 
-To build Scoobi:
-
-      $ cd scoobi
-      $ sbt publish-local
-
-Then build and package one of the examples:
+To build a scoobi application (e.g. the included wordCount example):
 
       $ cd examples/wordCount
       $ sbt package-hadoop
@@ -32,5 +28,9 @@ Then build and package one of the examples:
 Finally, run on Hadoop:
 
       $ hadoop jar ./target/Scoobi_Word_Count-hadoop-0.1.jar <input> <output>
-                                                                                                                        """
+
+Note that there appears to be an OSX-specific [issue](https://github.com/NICTA/scoobi/issues/1) associated with calling hadoop in this manner requiring the jar to be added to HADOOP_CLASSPATH and then hadoop being given the correct object to run. e.g.:
+
+      $ export HADOOP_CLASSPATH=$PWD/target/Scoobi_Word_Count-hadoop-0.1.jar
+      $ hadoop WordCount inputFile/to/wordcount nonexistent/outputdir                                                                                                                """
 }
