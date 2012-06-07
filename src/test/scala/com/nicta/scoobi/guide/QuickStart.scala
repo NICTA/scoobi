6a4ade092c4486a56e2a79405dd425c0668a483d
Russell Aronson <russell.aronson@gmail.com>
2012-06-07 10:36:37 +1000

    Fix getting started guide example

--- src/test/scala/com/nicta/scoobi/guide/QuickStart.scala
+++ src/test/scala/com/nicta/scoobi/guide/QuickStart.scala
@@ -66,10 +66,12 @@ object WordCount extends ScoobiApp {
   def run() {
     val lines = fromTextFile(args(0))
 
-        val counts = lines.flatMap(_.split(" "))
-                          .map(word => (word, 1))
-                          .groupByKey
-                          .combine((a: Int, b: Int) => a + b)
+    val counts = lines.flatMap(_.split(" "))
+                      .map(word => (word, 1))
+                      .groupByKey
+                      .combine((a: Int, b: Int) => a + b)
+    
+    persist(toTextFile(counts, args(1)))
   }
 }
 ```  
