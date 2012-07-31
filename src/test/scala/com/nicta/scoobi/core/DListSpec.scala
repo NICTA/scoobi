5207a04ca417e9c417c1236cd625adb5a23597ab
Eric Torreborre <etorreborre@yahoo.com>
2012-07-31 16:19:06 +1000

    Throwing a more descriptive error message when the reduce operation is called on an empty DList
    Fixes #104

--- src/test/scala/com/nicta/scoobi/core/DListSpec.scala
+++ src/test/scala/com/nicta/scoobi/core/DListSpec.scala
@@ -6,9 +6,15 @@ import Scoobi._
 import testing.NictaSimpleJobs
 
 class DListSpec extends NictaSimpleJobs {
+
   tag("issue 99")
   "a DList can be created and persisted with some Text" >> { implicit sc: SC =>
     val list = DList((new Text("key1"), new Text("value1")), (new Text("key2"), new Text("value2")))
     run(list).sorted must_== Seq("(key1,value1)", "(key2,value2)")
+  }
+
+  tag("issue 104")
+  "Summing up an empty list should do something graceful" >> { implicit c: SC =>
+    persist(DList[Int]().sum) must throwAn[Exception](message = "the reduce operation is called on an empty list")
   }
 }
