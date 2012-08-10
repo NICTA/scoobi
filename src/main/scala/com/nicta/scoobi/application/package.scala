ee619bc0ea24ee577a472bf3273b6a292a5e99b1
Eric Torreborre <etorreborre@yahoo.com>
2012-08-10 10:26:48 +1000

    Fixed issue #113: taking the minimum and the maximum of a list
    The issue was in the Persister class were the relationship between DComp nodes and their optimised version was not properly tracked.
    More specifically, a DComp nodes were associated to its sinks and sinks to their optimised nodes. However in the case of materialised values
    there were no sinks so when looking for the result for a particular DComp, we ended up getting the result for another one.
    
    This whole issue indicates that we should find a better way to track the DComp <-> optimised nodes relationship.

--- src/main/scala/com/nicta/scoobi/application/package.scala
+++ src/main/scala/com/nicta/scoobi/application/package.scala
@@ -1,5 +1,8 @@
 package com.nicta.scoobi
-package object application {
+
+import org.apache.hadoop.io.{DoubleWritable, BytesWritable, Text}
+
+package object application extends Orderings {
 
   // see the use of tagged types: http://etorreborre.blogspot.com.au/2011/11/practical-uses-for-unboxed-tagged-types.html
   // this will be replaced with similar functionalities in scalaz 7 when released
@@ -9,4 +12,19 @@ package object application {
   trait AsLevel
   type Level = String @@ AsLevel
   def level(l: String): Level = l.asInstanceOf[Level]
+
+}
+
+trait Orderings {
+  implicit def TextOrdering = new Ordering[Text] {
+    def compare(x: Text, y: Text): Int = x.compareTo(y)
+  }
+
+  implicit def BytesOrdering = new Ordering[BytesWritable] {
+    def compare(x: BytesWritable, y: BytesWritable): Int = x.compareTo(y)
+  }
+
+  implicit def DoubleOrdering = new Ordering[DoubleWritable] {
+    def compare(x: DoubleWritable, y: DoubleWritable): Int = x.compareTo(y)
+  }
 }
