cb1c171c881a18c24e8387dc403eeb05a12a0eb5
Russell Aronson <russell.aronson@gmail.com>
2012-08-14 14:07:35 +1000

    Added extra test case for issue #127
    Now also verifying that extra GBK nodes between a Materialize and a
    common predecessor in the graph doesn't cause a stack overflow.

--- src/test/scala/com/nicta/scoobi/acceptance/MultipleMscrSpec.scala
+++ src/test/scala/com/nicta/scoobi/acceptance/MultipleMscrSpec.scala
@@ -70,4 +70,22 @@ class MultipleMscrSpec extends NictaSimpleJobs {
         (expectedGBKs, ("v1",Seq(("k1","v1")))),
         (expectedGBKs, ("v2",Seq(("k2","v2")))))
   }
+
+  "A DList grouped in two different ways with one of them grouped again, materialized, then joined to the other." >> { implicit c: SC =>
+
+    val input = fromDelimitedInput("k1,v1","k2,v2").collect { case key :: value :: _ => (key, value) }
+
+    val inputGrouped = input.groupBy(_._1).map(a => a).groupBy(_._1).map(b => (b._1, b._2.flatMap(_._2)) )
+    val inputGroupedDifferently = input.groupBy(_._2)
+
+    val inputGroupedAsDObject = inputGrouped.materialize
+
+    val dObjectJoinedToInputGroupedDiff = (inputGroupedAsDObject join inputGroupedDifferently)
+
+    val expectedGBKs = Seq(("k1",Seq(("k1","v1"))), ("k2",Seq(("k2","v2"))))
+
+    persist(dObjectJoinedToInputGroupedDiff.materialize).toSeq must_== Seq(
+        (expectedGBKs, ("v1",Seq(("k1","v1")))),
+        (expectedGBKs, ("v2",Seq(("k2","v2")))))
+  }
 }
