d0f2933cb4f4c97eaf284648b6bf5e25daf30c96
Ben Wing <ben@benwing.com>
2012-08-14 03:01:38 -0400

    integrated parts of @benwing documentation contribution

--- src/test/scala/com/nicta/scoobi/guide/Testing.scala
+++ src/test/scala/com/nicta/scoobi/guide/Testing.scala
@@ -18,11 +18,23 @@ package guide
 
 class Testing extends ScoobiPage { def is = "Testing guide".title^
   """
-### Introduction
+### Running locally
+
+The first thing to do is to run your code locally (in non-distributed mode). This does not require you to be running on a Hadoop cluster or even to have Hadoop installed on your local machine.  The way to do that is simply to run your application normally under the JVM.  You can do that directly using `java`, but specifying the right class path is tricky; as a result, it's often easier to run using `sbt run-main`.  For example, if your main class is called `mypackage.myapp.RunMyApp`, run something like the following command from the top-level directory of your SBT project:
+
+    sbt run-main mypackage.myapp.RunMyApp input-files output
+
+### Running on the cluster
+
+Only when your application runs and works locally should you try running on a Hadoop cluster.  In order to do so you will probably need to build an assembly, i.e. a self-contained JAR file containing all of the libraries needed.  This can be done using the `sbt-assembly` plugin to SBT.  See the [Quick Start](${SCOOBI_GUIDE_PAGE}Quick%20Start.html) page in this User Guide for more information.
+
+Once you have built the assembly, you can run it by logging into the job tracker and running it using `hadoop`, e.g. as follows:
+
+    hadoop jar ./target/MyApp-assembly-0.1.jar mypackage.myapp.RunMyApp input-files output
+
+### With specs2
 
 Scoobi provides testing support to make your coding experience as productive as possible. It is tightly integrated with [specs2](http://specs2.org) out-of-the-box, but you can reuse the testing traits with your own favorite testing library.
-
-### Using specs2
 
 First of all you need to add the specs2 dependency to your build.sbt file (use the same version that [Scoobi is using](https://github.com/NICTA/scoobi/tree/${SCOOBI_BRANCH}/build.sbt))
 
@@ -106,7 +118,7 @@ Passing the configuration to each example is a bit verbose so you can use a type
     class WordCountSpec extends HadoopSpecification {
       type SC = ScoobiConfiguration
 
-      "Counting words frequencies must return the frequency for each word" >> { conf: SC =>
+      "Counting words frequencies must return the frequency for each word" >> { implicit conf: SC =>
         // your Scoobi code goes here
       }
     }
@@ -138,7 +150,7 @@ The `HadoopSpecification` class allows to create any kind of job and execute the
       forall(odds.map(_.toInt))(i => i must beOdd)
     }
 
-### Using your own
+### With your own library
 
 Some of the functionalities described above has been extracted into traits in the `application` package which you can reuse with your own test framework:
 
@@ -152,6 +164,28 @@ Some of the functionalities described above has been extracted into traits in th
 
  * `LibJars` distributes the dependent jars to the cluster
 
+### Debugging tips
+
+Here are a few tips to help you in debugging your application:
+
+ - *don't use shared state!* Since Hadoop is executing distributed computations, there is no way to share the content of variables across different JVMs. So if one of the functions that you use on a DList updates a variable somewhere, you will get incorrect results or NullPointerExceptions. This also goes for external libraries. Some of them can store some local state which will cause issues in a distributed mode
+
+
+ - allow some tasks to fail without the entire job failing by using the settings `mapred.max.map.failures.percent` and `mapred.max.reduce.failures.percent`.  For example, the following command
+
+      `hadoop jar ./myapp.jar mypackage.RunMyApp -Dmapred.max.map.failures.percent=20 -Dmapred.max.reduce.failures.percent=20 input output`
+
+     will allow up to 20% of map and reduce tasks to fail while still allowing the operation as a whole to proceed
+
+ - Scoobi logs additional debugging information at the `DEBUG` logging level.  Unfortunately, Apache Commons Logging provides no means of programmatically changing the logging level.  To do this you have to reach down directly into `log4j`, the underlying logging implementation used in Hadoop, as follows:
+
+        import org.apache.log4j.{Level=>JLevel,_}
+
+        {
+          ...
+          LogManager.getRootLogger().setLevel(JLevel.DEBUG.asInstanceOf[JLevel])
+          ...
+        }
   """
 
 }
