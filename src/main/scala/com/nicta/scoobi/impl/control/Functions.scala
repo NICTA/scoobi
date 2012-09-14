6dfeca9815c0a9dcc7370761562b197b1103a122
etorreborre <etorreborre@yahoo.com>
2012-09-14 16:41:25 +1000

    added logical combinators on functions returning booleans

---
+++ src/main/scala/com/nicta/scoobi/impl/control/Functions.scala
@@ -0,0 +1,16 @@
+package com.nicta.scoobi
+package impl
+package control
+import scalaz.Scalaz._
+
+trait Functions {
+  implicit def logicalFunction[A](f: A => Boolean): LogicalFunction[A] = LogicalFunction(f)
+
+  case class LogicalFunction[A](f: A => Boolean) {
+    def ||(g: A => Boolean) = (a: A) => f(a) || g(a)
+    def &&(g: A => Boolean) = (a: A) => f(a) && g(a)
+    def unary_!             = (a: A) => !f(a)
+  }
+}
+
+object Functions extends Functions
