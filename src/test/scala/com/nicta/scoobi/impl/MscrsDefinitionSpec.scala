f840ed551d82b3b738569b9dcc3f24dcb7ffbc0b
etorreborre <etorreborre@yahoo.com>
2012-11-23 00:21:39 +1100

    added a spec to create the gbk output channels

--- src/test/scala/com/nicta/scoobi/impl/MscrsDefinitionSpec.scala
+++ src/test/scala/com/nicta/scoobi/impl/MscrsDefinitionSpec.scala
@@ -7,6 +7,7 @@ import org.scalacheck.Prop._
 import org.specs2._
 import org.specs2.collection.Iterablex._
 import plan.comp.GroupByKey
+import plan.mscr.GbkOutputChannel
 import specification.Groups
 import matcher.ThrownExpectations
 import plan.comp._
@@ -14,22 +15,64 @@ import core.CompNode
 
 class MscrsDefinitionSpec extends Specification with Groups with ThrownExpectations with CompNodeData { def is =
 
- "The gbks of a graph can be sorted in a topological sort" ^
-   "all the nodes in a layer cannot be parent of each other"       ! g1().e1^
-   "2 different layers have at least 2 nodes parent of each other" ! g1().e2^
-   end
+  "The gbks of a graph can be sorted in a topological sort" ^
+    "all the nodes in a layer cannot be parent of each other"       ! g1().e1^
+    "2 different layers have at least 2 nodes parent of each other" ! g1().e2^
+    p^
+    p^
+  "For each layer in the topological sort, we can create Mscrs"^
+    "each gbk belongs to a GbkOutputChannel"                      ! g2().e1^
+    "aggregating the flatten node if there is one before the gbk" ! g2().e2^
+    "aggregating the combine node if there is one after the gbk" ! g2().e3^
+    "aggregating the pd node if there is one after the gbk"      ! g2().e4^
+    "aggregating the combine and pd nodes if they are after the gbk" ! g2().e5^
+    endp^
+    "GbkOutputChannels have inputs, some of them are Mappers"
+    "All mappers sharing the same input go to the same MapperInputChannel"
+    "Other Gbk inputs go to an IdInputChannel"
+    "GbkOutputChannels sharing the same MapperInputChannels belong to the same Mscr"
+
 
   "topological sort" - new g1 with definition {
     e1 := prop { layer: Layer[GBK] =>
       val nodes = layer.nodes
       nodes.forall(n => !nodes.exists(_ -> isStrictParentOf(n))) ==== true
-    }
+    }.set(minTestsOk -> 10)
     e2 := forAll(genLayerPair) { (pair: (Layer[GBK], Layer[GBK])) =>
       val (layer1, layer2) = pair
       (layer1 ==== layer2) or
       (layer1.nodes.exists(n1 => layer2.nodes.exists(_ -> isStrictParentOf(n1))) ==== true)
-    }.set(minTestsOk -> 30)
+    }.set(minTestsOk -> 5)
   }
+
+  "output channels" - new g2 with definition {
+    e1 := {
+      val gbk1 = gbk(load)
+      (gbk1 -> gbkOutputChannel) === GbkOutputChannel(gbk1)
+    }
+    e2 := {
+      val fl1 = flatten(load)
+      val gbk1 = gbk(fl1)
+      (gbk1 -> gbkOutputChannel) === GbkOutputChannel(gbk1, flatten = Some(fl1))
+    }
+    e3 := {
+      val gbk1 = gbk(load)
+      val cb1 = cb(gbk1)
+      (gbk1 -> gbkOutputChannel) === GbkOutputChannel(gbk1, combiner = Some(cb1))
+    }
+    e4 := {
+      val gbk1 = gbk(load)
+      val pd1 = pd(gbk1)
+      (gbk1 -> gbkOutputChannel) === GbkOutputChannel(gbk1, reducer = Some(pd1))
+    }
+    e5 := {
+      val gbk1 = gbk(load)
+      val cb1 = cb(gbk1)
+      val pd1 = pd(cb1)
+      (gbk1 -> gbkOutputChannel) === GbkOutputChannel(gbk1, combiner = Some(cb1), reducer = Some(pd1))
+    }
+  }
+
 
   trait definition extends factory with MscrsDefinition with ShowNode {
     implicit val arbitraryLayer: Arbitrary[Layer[GBK]] = Arbitrary(genLayer)
@@ -44,6 +87,17 @@ class MscrsDefinitionSpec extends Specification with Groups with ThrownExpectati
   }
   trait MscrsDefinition extends CompNodes with TopologicalSort[GroupByKey[_,_]] {
     def selectNode(n: CompNode) = !Seq(n).collect(isAGroupByKey).isEmpty
+
+    lazy val gbkOutputChannel: GBK => GbkOutputChannel = attr { case g  =>
+      val flatten = Seq(g.in).collect(isAFlatten).headOption
+
+      (g -> ancestors).toList match {
+        case (c @ Combine1(_)) :: (p @ ParallelDo1(_)) :: rest => GbkOutputChannel(g, flatten, combiner = Some(c), reducer = Some(p))
+        case (c @ Combine1(_)) :: rest                         => GbkOutputChannel(g, flatten, combiner = Some(c))
+        case (p @ ParallelDo1(_)) :: rest                      => GbkOutputChannel(g, flatten, reducer = Some(p))
+        case _                                                 => GbkOutputChannel(g)
+      }
+    }
   }
 
 }
