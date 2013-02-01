aa2e2a27ca9453a44f7e7b4d0fef7515c700a126
etorreborre <etorreborre@yahoo.com>
2013-02-01 15:57:56 +1100

    change in the command line for test-only

--- project/build.scala
+++ project/build.scala
@@ -174,7 +174,7 @@ object build extends Build {
     st.log.info("Generating the documentation")
     val extracted = Project.extract(st)
     val ref: ProjectRef = extracted.get(thisProjectRef)
-    val testState = reapply(Seq[Setting[_]](testOptions += Tests.Argument("include", "notfound")), st)
+    val testState = reapply(Seq[Setting[_]](testOptions ++= Seq(Tests.Filter(name => false), Tests.Argument("include notfound"))), st)
     extracted.runTask(test in Test in ref, testState)._1
   }
 
