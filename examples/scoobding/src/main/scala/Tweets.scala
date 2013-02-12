59eb8a3cd88f6fb292967cd61164ca36d45f9b18
etorreborre <etorreborre@yahoo.com>
2013-02-13 09:42:16 +1100

    add an example of equivalent java code for the scoobding example

--- examples/scoobding/src/main/scala/Tweets.scala
+++ examples/scoobding/src/main/scala/Tweets.scala
@@ -1,10 +1,12 @@
 import com.nicta.scoobi.Scoobi._
 import app.Histogram._
-import scalaz.Scalaz._
 
+/**
+ * A Scoobi application to display the number of tweets per tweet message size
+ */
 object Tweets extends ScoobiApp {
 
-  def run {
+  def run() {
     fromTextFile("tweets.txt").
       map(t => (t.size, 1)).
       groupByKey.
@@ -14,3 +16,73 @@ object Tweets extends ScoobiApp {
 
 }
 
+/**
+ * Equivalent Java/Hadoop code
+
+package org.myorg;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class TweetCount {
+
+ public static class Map extends Mapper<LongWritable, Text, IntWritable, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private IntWritable messageSize = new IntWritable();
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        StringTokenizer tokenizer = new StringTokenizer(line);
+        while (tokenizer.hasMoreTokens()) {
+            String message = tokenizer.nextToken()
+            messageSize.set(message.length);
+            context.write(messageSize, one);
+        }
+    }
+ }
+
+ public static class Reduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        context.write(key, new IntWritable(sum));
+    }
+ }
+
+ public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+
+    Job job = new Job(conf, "tweetcount");
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+
+    job.setMapperClass(Map.class);
+    job.setReducerClass(Reduce.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.waitForCompletion(true);
+ }
+
+}
+
+
+ */
