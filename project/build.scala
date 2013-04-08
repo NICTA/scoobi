1991fe5f584c805cb57b9d17169e973a27984bfa
etorreborre <etorreborre@yahoo.com>
2013-04-08 14:45:55 +1000

    Revert "removing the avro example project from the main build until dependencies are sorted out. fixes #232"
    This reverts commit f09f9866dbb9b087f7f111afa7b80a88ce4df114.

--- project/build.scala
+++ project/build.scala
@@ -181,6 +181,7 @@ object build extends Build {
    * EXAMPLE PROJECTS
    */
   def project(name: String) = Project(id = name, base = file("examples/"+name))
+  lazy val avro      = project("avro")
   lazy val fatjar    = project("fatjar")
   lazy val pageRank  = project("pageRank")
   lazy val scoobding = project("scoobding")
