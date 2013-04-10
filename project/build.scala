e092b59a14c4a409cc5f4525cae3988d5bb59353
etorreborre <etorreborre@yahoo.com>
2013-04-10 13:13:13 +1000

    trying to add a bit more memory to the tests

--- project/build.scala
+++ project/build.scala
@@ -106,7 +106,7 @@ object build extends Build {
     testOptions := Seq(Tests.Filter(s => s.endsWith("Spec") || Seq("Index", "All", "UserGuide", "ReadMe").exists(s.contains))),
     // run each test in its own jvm
     fork in Test := true,
-    javaOptions in Test ++= Seq("-Xmx1g")
+    javaOptions in Test ++= Seq("-Xmx1500m")
   )
 
   lazy val siteSettings: Seq[Settings] = ghpages.settings ++ SbtSite.site.settings ++ Seq(
