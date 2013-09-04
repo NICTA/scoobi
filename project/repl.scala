3686bbf3231a35e41f1942b8a90b91f724012118
markhibberd <mark@hibberd.id.au>
2013-09-04 17:16:37 +1000

    [#292] Add `repl:dist` to build a tar ball containing the script and fat jar.

--- project/repl.scala
+++ project/repl.scala
@@ -24,6 +24,8 @@ object repl {
 
   lazy val Repl = config("repl")
 
+  lazy val dist = TaskKey[Unit]("dist")
+
   lazy val settings: Seq[Settings] =
     inConfig(Repl)(assemblySettings) ++
     inConfig(Repl)(Defaults.projectBaseSettings) ++
@@ -32,7 +34,7 @@ object repl {
   lazy val deps =
     libraryDependencies in Repl :=
       dependencies.scoobi(scalaVersion.value)                ++
-      dependencies.hadoop(version.value).map(_ % "provided") ++
+      dependencies.hadoop(version.value).map(dehadoop)       ++
       dependencies.scalaz()                                  ++
       dependencies.specs2()                                  ++
       dependencies.repl
@@ -42,7 +44,8 @@ object repl {
     fullClasspath in (Repl, assembly)   <<= (update in Repl, exportedProducts in Compile) map (jars),
     jarName       in (Repl, assembly)   <<= (version in ThisBuild) map { v => s"scoobi-repl-$v.jar" },
     test          in (Repl, assembly)   :=  {},
-    mergeStrategy in (Repl, assembly)   <<= mergeStrategy in (Repl, assembly) apply (merge)
+    mergeStrategy in (Repl, assembly)   <<= mergeStrategy in (Repl, assembly) apply (merge),
+    dist          in Repl               <<= mkdist
   )
 
   def jars(report: UpdateReport, main: Classpath) =
@@ -53,4 +56,31 @@ object repl {
       val oldstrat = old(x)
       if (oldstrat == MergeStrategy.deduplicate) MergeStrategy.first  else oldstrat
     }
+
+  def dehadoop(module: ModuleID) =
+    if (module.organization == "org.apache.hadoop") module % "provided" else module
+
+  def mkdist =
+    (assembly in (Repl, assembly), target, version in ThisBuild).map((assembly, target, version) => {
+      val files = Seq(assembly, file("src/main/bin/scoobi"))
+      val distname = s"scoobi-repl-${version}"
+      val distdir = target / distname
+      val disttar = target / s"${distname}.tar.gz"
+      (files x flat).foreach({
+        case (src, trg) =>
+          IO.copyFile(src, distdir / trg)
+      })
+      Process(Seq("chmod", "+x", (distdir / "scoobi").getAbsolutePath)).! match {
+        case 0 => ()
+        case n => sys.error(s"Failed to chmod scoobi [$n]")
+      }
+      Process(Seq("ln", "-fs", assembly.getName, "scoobi-repl.jar"), Some(distdir)).! match {
+        case 0 => ()
+        case n => sys.error(s"Failed to create link [$n]")
+      }
+      Process(Seq("tar", "cfz", disttar.getAbsolutePath, "-C", target.getAbsolutePath, distname)).! match {
+        case 0 => ()
+        case n => sys.error(s"Failed to create tar [$n]")
+      }
+    })
 }
