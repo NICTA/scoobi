90b33e2a68277239b9927ddd488f049711d866c6
Charles O'Farrell <charleso@charleso.org>
2014-07-22 14:31:09 +1000

    Fix delayed scoobi> prompt in 2.11

--- src/main/scala-2.11/com/nicta/scoobi/application/ILoopCompat.scala
+++ src/main/scala-2.11/com/nicta/scoobi/application/ILoopCompat.scala
@@ -6,5 +6,7 @@ import scala.tools.nsc.interpreter.ILoop
 // There doesn't appear to be a common function between 2.10 and 2.11 that will preserve the same behaviour
 trait ILoopCompat extends ILoop {
 
-  def addThunk(f: => Unit): Unit = f
+  def addThunk(f: => Unit): Unit =
+  // Evaluating 'f' directly still works, but the scoobi> prompt is shown immediately after "press Enter"
+    intp.initialize(f)
 }
