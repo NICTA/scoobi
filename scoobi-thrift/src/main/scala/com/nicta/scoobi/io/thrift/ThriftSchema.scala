2c5c53f90527f84a4cf13eb7e43a98384e86b6c6
Eric Torreborre <etorreborre@yahoo.com>
2014-09-01 17:27:13 +1000

    Revert "Merge pull request #341 from NICTA/topic/thrift-serialiser-thread-safe"
    This reverts commit 7005ef5b46344b6957795d0217d8dede045cf1c4, reversing
    changes made to c9d3fefc1d79298288ad8cd70a81a3d78c35b9a0.

--- scoobi-thrift/src/main/scala/com/nicta/scoobi/io/thrift/ThriftSchema.scala
+++ scoobi-thrift/src/main/scala/com/nicta/scoobi/io/thrift/ThriftSchema.scala
@@ -12,11 +12,12 @@ object ThriftSchema {
 
   /* WARNING THIS MUST BE A DEF OR OR IT CAN TRIGGER CONCURRENCY ISSUES WITH SHARED THRIFT SERIALIZERS */
   def mkThriftFmt[A](implicit m: Manifest[A], ev: A <:< ThriftLike): WireFormat[A] = new WireFormat[A] {
+    val serialiser = ThriftSerialiser()
     // Call once when the implicit is created to avoid further reflection
     val empty = m.runtimeClass.newInstance().asInstanceOf[A]
 
     def toWire(x: A, out: DataOutput) = {
-      val bytes = ThriftSerialiser().toBytes(x)
+      val bytes = serialiser.toBytes(x)
       out.writeInt(bytes.length)
       out.write(bytes)
     }
@@ -25,7 +26,7 @@ object ThriftSchema {
       val size = in.readInt()
       val bytes = new Array[Byte](size)
       in.readFully(bytes)
-      ThriftSerialiser().fromBytes(empty, bytes)
+      serialiser.fromBytes(empty, bytes)
     }
 
     override def toString = "ThriftObject"
@@ -34,12 +35,13 @@ object ThriftSchema {
   /* WARNING THIS MUST BE A DEF OR OR IT CAN TRIGGER CONCURRENCY ISSUES WITH SHARED THRIFT SERIALIZERS*/
   def mkThriftSchema[A](implicit m: Manifest[A], ev: A <:< ThriftLike) = new SeqSchema[A] {
     type SeqType = BytesWritable
+    val serialiser = ThriftSerialiser()
     // Call once when the implicit is created to avoid further reflection
     val empty = m.runtimeClass.newInstance().asInstanceOf[A]
 
-    def toWritable(x: A) = new BytesWritable(ThriftSerialiser().toBytes(x))
+    def toWritable(x: A) = new BytesWritable(serialiser.toBytes(x))
 
-    def fromWritable(x: BytesWritable): A = ThriftSerialiser().fromBytes(empty, x.getBytes)
+    def fromWritable(x: BytesWritable): A = serialiser.fromBytes(empty, x.getBytes)
 
     val mf: Manifest[SeqType] = implicitly
   }
