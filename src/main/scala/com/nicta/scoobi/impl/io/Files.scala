NICTA/scoobi

commit e956c7e5bd8f4f49811f64771c601fecd37ca15c

don't move files which are already in the right place

src/main/scala/com/nicta/scoobi/impl/io/Files.scala
@@ -54,7 +54,7 @@ trait Files {
       if (!pathExists(destPath.getParent)) to.mkdirs(destPath.getParent)
 
       if (sameFileSystem(from, to))
-        (path == destPath) || {
+        path.toUri.getPath.startsWith(destPath.toUri.getPath) || {
           logger.debug(s"renaming $path to $destPath")
           tryOk {
             Compatibility.rename(path, destPath)

commit a01c4e9a00abcba42ff21fecd75264f3dc44f313

fixed the renaming bug introduced by e956c7e

src/main/scala/com/nicta/scoobi/impl/io/Files.scala
@@ -54,7 +54,9 @@ trait Files {
       if (!pathExists(destPath.getParent)) to.mkdirs(destPath.getParent)
 
       if (sameFileSystem(from, to))
-        path.toUri.getPath.startsWith(destPath.toUri.getPath) || {
+        (path == destPath) || // same files
+        (from.isDirectory(path) && to.isDirectory(destPath) && path.toUri.getPath.startsWith(destPath.toUri.getPath)) || // nested directories
+        {
           logger.debug(s"renaming $path to $destPath")
           tryOk {
             Compatibility.rename(path, destPath)